f99b18e861ad445619392a90bc4dd2707beb1b0f
Oleg Fedotov <trevor706@gmail.com>
2022-09-05 18:09:34 +0300

    NetworkDataFetcher class

---
+++ Bitcoin Price/NetworkDataFetcher.swift
@@ -0,0 +1,30 @@
+//
+//  NetworkDataFetcher.swift
+//  Bitcoin Price
+//
+//  Created by Olegio on 05.09.2022.
+//
+
+import Foundation
+
+class NetworkDataFetcher {
+    
+    let networkService = NetworkService()
+    
+    func fetchData(urlString: String, responce: @escaping (SearchResponce?) -> Void) {
+        networkService.request(urlString: urlString) { result in
+            switch result {
+            case .success(let data):
+                do {
+                    let currencyes = try JSONDecoder().decode(SearchResponce.self, from: data)
+                    responce(currencyes)
+                } catch let jsonError{
+                    print("Failed to decode json", jsonError)
+                }
+            case .failure(let error):
+                print("Error recieved requesting data: \(error.localizedDescription)")
+                responce(nil)
+            }
+        }
+    }
+}
