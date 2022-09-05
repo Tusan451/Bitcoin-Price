9c0c7af2104dd3cd4b102a878696e1ee860e90f0
Oleg Fedotov <trevor706@gmail.com>
2022-09-05 16:26:47 +0300

    Initial Commit

---
+++ Bitcoin Price/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bitcoin Price
+//
+//  Created by Olegio on 05.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
